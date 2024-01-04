6a826732f0c8998e4ed17cf6bef731e175f389df
Mohammad Azarbayejani <azarmoh81@gmail.com>
2024-01-04 16:25:57 +0330

    a bug fixed :)

--- show_channels.v
+++ show_channels.v
@@ -42,19 +42,19 @@ module show_channels(
 	end
 
     // ----- Send channel address to SPI -----
-    assign channel_addr_to_SPI[0:0] <= channel_addr_reg[0:0];
-    assign channel_addr_to_SPI[1:1] <= channel_addr_reg[1:1];
-    assign channel_addr_to_SPI[2:2] <= channel_addr_reg[2:2];
+    assign channel_addr_to_SPI[0:0] = channel_addr_reg[0:0];
+    assign channel_addr_to_SPI[1:1] = channel_addr_reg[1:1];
+    assign channel_addr_to_SPI[2:2] = channel_addr_reg[2:2];
 
     // ----- Send channel address to LED -----
-    assign led[0] <= channel_addr[0:0];
-    assign led[1] <= channel_addr[1:1];
-    assign led[2] <= channel_addr[2:2];
-    assign led[3] <= channel_addr[3:3];
-    assign led[4] <= channel_addr[4:4];
-    assign led[5] <= channel_addr[5:5];
-    assign led[6] <= channel_addr[6:6];
-    assign led[7] <= channel_addr[7:7];
+    assign led[0] = channel_addr[0:0];
+    assign led[1] = channel_addr[1:1];
+    assign led[2] = channel_addr[2:2];
+    assign led[3] = channel_addr[3:3];
+    assign led[4] = channel_addr[4:4];
+    assign led[5] = channel_addr[5:5];
+    assign led[6] = channel_addr[6:6];
+    assign led[7] = channel_addr[7:7];
 	
     // assign led[0] = (channel_addr_reg == 3'b000) ? 1:0;
     // assign led[1] = (channel_addr_reg == 3'b001) ? 1:0;
