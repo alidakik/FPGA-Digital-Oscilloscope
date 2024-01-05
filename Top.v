64f2442201e8b4d6fd4d5ab8cfc0dedeb286a7e4
Mohammad Azarbayejani <azarmoh81@gmail.com>
2024-01-05 13:20:42 +0330

    some Wire and Register edited

--- Top.v
+++ Top.v
@@ -19,28 +19,27 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Top(
-		input Clk_pin,
-		input Resetn_pin,
+		input  Clk_pin,
+		input  Resetn_pin,
 
 		//-- Adc interfaces ---
-		input Adc2Fpga_data,
-		output reg Fpga2Adc_data,
-		output reg Fpga2Adc_Csn,
-		output reg Fpga2Adc_sclk,
-		output reg sclk,
+		input  Adc2Fpga_data,
+		output Fpga2Adc_data,
+		output Fpga2Adc_Csn,
+		output Fpga2Adc_sclk,
+		output sclk,
+		
 		//--- 7-segment --- 
-		output reg [7:0] sseg,
-		output reg [3:0] sseg_En,
+		output [7:0] sseg,
+		output [3:0] sseg_En,
 
 		//  --- LED ---
-		output reg [7:0] LED,
-		input [7:0] DipSwitch,
+		output [7:0] LED,
+		input  [7:0] DipSwitch,
 
 		output dcm_locked
     );
 	
-	reg Adc2Fpga_data_reg = Adc2Fpga_data;
-	reg [7:0] DipSwitch_reg = DipSwitch;
 	wire [2:0] ADDR;
 	wire [11:0] ADC2Sseg;
 	
@@ -51,45 +50,44 @@ module Top(
 	
 	//DONE
 	clk_generator clk_generator
-   (// Clock in ports
-    .CLK_IN1(Clk_pin),      // IN
+   (// Clock in portsS
+    .CLK_IN1(Clk_pin),       // IN
     // Clock out ports
-    .CLK_OUT1(Clk_12),     // OUT
-    .CLK_OUT2(Clk_24),     // OUT
+    .CLK_OUT1(Clk_12),       // OUT
+    .CLK_OUT2(Clk_24),       // OUT
     // Status and control signals
-    .RESET(Resetn_pin),// IN
-    .LOCKED(LOCKED),       // OUT
-    .CLK_VALID(CLK_VALID));   // OUT
+    .RESET(Resetn_pin),      // IN
+    .LOCKED(LOCKED),         // OUT
+    .CLK_VALID(CLK_VALID));  // OUT
 	 
 	 
 	 //DONE
 	 SPI spi (
-    .clk(Clk_12), 
-    .sclk(Fpga2Adc_sclk), 
-    .din(Fpga2Adc_data), 
-    .cs(Fpga2Adc_Csn), 
-    .ADD(ADDR), 
-    .ADC2SPI(Adc2Fpga_data), 
-    .ADC2Sseg(ADC2Sseg), 
-    //.ready(ready)
+    .clk(Clk_12),           // IN
+    .sclk(Fpga2Adc_sclk), 	 // OUT
+    .din(Fpga2Adc_data),    // OUT
+    .cs(Fpga2Adc_Csn),      // OUT
+    .ADD(ADDR),             // IN
+    .ADC2SPI(Adc2Fpga_data),// IN
+    .ADC2Sseg(ADC2Sseg)     // OUT
     );
 
 	//DONE
 	S_segment_displayer sseg_displayer (
-    .en(en), 
-    .svn_conf(sseg[7:1]), 
-    .DP(sseg[0]), 
-    .data(ADC2Sseg), 
-    .clk(Clk_12)
+    .en(sseg_En),           // OUT
+    .svn_conf(sseg[7:1]),   // OUT
+    .DP(sseg[0]),           // OUT
+    .data(ADC2Sseg), 		 // IN
+    .clk(Clk_12)            // IN
     );
 	
 	//DONE
 	show_channels led_displayer (
-    .clk(Clk_12), 
-    .resetn(Resetn_pin), 
-    .channel_addr(DipSwitch_reg), 
-    .led(LED), 
-    .channel_addr_to_SPI(ADDR) //vasl she
+    .clk(Clk_12),                // IN
+    .resetn(Resetn_pin),         // IN
+    .channel_addr(DipSwitch),    // IN
+    .led(LED),                   // OUT
+    .channel_addr_to_SPI(ADDR)   // OUT
     );
 
 endmodule
