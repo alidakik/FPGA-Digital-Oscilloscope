87973ceae3a14a4ebbf671913c4e13b4f52b1ff2
Mohammad Azarbayejani <azarmoh81@gmail.com>
2024-01-05 22:50:07 +0330

    a bug fixed

--- show_channels.v
+++ show_channels.v
@@ -21,10 +21,10 @@
 module show_channels(
     input clk,
     input resetn,
-    input [7:0] channel_addr,             //from DIP switch PIN
+    input [7:0] channel_addr,              //from DIP switch PIN
     // input [2:0] channel_addr,
-    output reg [7:0] led,                 //Output to LED PIN
-    output reg [2:0] channel_addr_to_SPI  //Output to SPI
+    output reg  [7:0] led,                 //Output to LED PIN
+    output [2:0] channel_addr_to_SPI       //Output to SPI
     );
 	
 	reg [2:0] channel_addr_reg; 
@@ -47,15 +47,12 @@ module show_channels(
     assign channel_addr_to_SPI[2:2] = channel_addr_reg[2:2];
 
     // ----- Send channel address to LED -----
-    assign led[0] = channel_addr[0:0];
-    assign led[1] = channel_addr[1:1];
-    assign led[2] = channel_addr[2:2];
-    assign led[3] = channel_addr[3:3];
-    assign led[4] = channel_addr[4:4];
-    assign led[5] = channel_addr[5:5];
-    assign led[6] = channel_addr[6:6];
-    assign led[7] = channel_addr[7:7];
-	
+	 
+	 
+	always@(posedge clk)
+	begin 
+        led <= channel_addr_reg;
+	end 
     // assign led[0] = (channel_addr_reg == 3'b000) ? 1:0;
     // assign led[1] = (channel_addr_reg == 3'b001) ? 1:0;
     // assign led[2] = (channel_addr_reg == 3'b010) ? 1:0;
